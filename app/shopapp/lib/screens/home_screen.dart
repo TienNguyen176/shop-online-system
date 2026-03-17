a3b5669056c215946b697297cbc51fc3562a0d56
Tien-Van-176 <furinawibu176@gmail.com>
2026-03-18 05:47:00 +0700

    fix refesh home

--- app/shopapp/lib/screens/home_screen.dart
+++ app/shopapp/lib/screens/home_screen.dart
@@ -125,9 +125,6 @@ class _HomeScreenState extends State<HomeScreen> {
     if (refresh) {
       page = 1;
       hasMore = true;
-
-      products.clear();
-      allProducts.clear();
     }
 
     if (!hasMore) return;
